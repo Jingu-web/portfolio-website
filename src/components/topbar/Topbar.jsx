0b6ccaf7579cabbc1b2b7aeaf9e217044dce65d5
Jingu-web <multiNomaFeBe.rjrrr@gmail.com>
2021-10-22 04:50:37 +0900

    💄 ロゴとGitHubアイコンの設置

--- src/components/topbar/Topbar.jsx
+++ src/components/topbar/Topbar.jsx
@@ -1,5 +1,21 @@
+import { GitHub } from "@material-ui/icons";
+
 import "./topbar.scss";
 
 export const Topbar = () => {
-  return <div className="topbar"></div>;
+  return (
+    <div className="topbar">
+      <div className="wrapper">
+        <div className="left">
+          <a href="#intro" className="logo">
+            Jin.
+          </a>
+          <div className="itemContainer">
+            <GitHub className="github-icon" />
+          </div>
+        </div>
+        <div className="right"></div>
+      </div>
+    </div>
+  );
 };
