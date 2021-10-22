173d59b0c28caf88ecc2cb3baf0d91b62268ae09
Jingu-web <multiNomaFeBe.rjrrr@gmail.com>
2021-10-22 10:52:10 +0900

    🤡 Portfolioリスト一覧のモックを作成

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -1,5 +1,25 @@
 import "./portfolio.scss";
 
 export const Portfolio = () => {
-  return <div className="portfolio" id="portfolio"></div>;
+  return (
+    <div className="portfolio" id="portfolio">
+      <h1>Portfolio</h1>
+      <ul>
+        <li className="active">Web App</li>
+        <li>Static Website</li>
+        <li>Desing</li>
+        <li>Proposal</li>
+      </ul>
+      <div className="container">
+        <div className="item">
+          <img src="" alt="" />
+          <h3>Sample App</h3>
+        </div>
+        <div className="item">
+          <img src="" alt="" />
+          <h3>Sample App</h3>
+        </div>
+      </div>
+    </div>
+  );
 };
