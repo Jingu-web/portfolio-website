d9e68716bee39d8f3de8dd99f662de6836d03e5a
Jingu-web <multiNomaFeBe.rjrrr@gmail.com>
2021-10-23 04:21:28 +0900

    💄 Contactページの作成

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,5 +1,35 @@
+import { useState } from "react";
+
 import "./contact.scss";
 
 export const Contact = () => {
-  return <div className="contact" id="contact"></div>;
+  const [message, setMesage] = useState(false);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setMesage(true);
+  };
+
+  return (
+    <div className="contact" id="contact">
+      <div className="left">
+        <img src="assets/contact.svg" alt="" />
+      </div>
+      <div className="right">
+        <h2>Contact</h2>
+        <form onSubmit={handleSubmit}>
+          <input type="text" placeholder="Email" />
+          <textarea placeholder="Message"></textarea>
+          <button type="submit">Send</button>
+          {message && (
+            <span>
+              ご連絡ありがとうございます。
+              <br />
+              メール確認次第追ってご連絡差し上げます。
+            </span>
+          )}
+        </form>
+      </div>
+    </div>
+  );
 };
