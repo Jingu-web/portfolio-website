6c698bec52799aececd4d131eb0dd721e253b336
Jingu-web <multiNomaFeBe.rjrrr@gmail.com>
2021-10-23 03:40:19 +0900

    🎨 ポートフォリオを挿入

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -1,9 +1,17 @@
+import { useState, useEffect } from "react";
+
 import "./portfolio.scss";
 import { PortfolioList } from "../portfolioList/portfolioList";
-import { useState } from "react";
+import {
+  webAppPortfolio,
+  staticWebsitePortfolio,
+  designPortfolio,
+  proposalPortfolio,
+} from "../../data";
 
 export const Portfolio = () => {
   const [selected, setSelected] = useState("webApp");
+  const [data, setData] = useState([]);
 
   const list = [
     {
@@ -24,6 +32,28 @@ export const Portfolio = () => {
     },
   ];
 
+  useEffect(() => {
+    switch (selected) {
+      case "webApp":
+        setData(webAppPortfolio);
+        break;
+      case "staticWebsite":
+        setData(staticWebsitePortfolio);
+        break;
+      case "design":
+        setData(designPortfolio);
+        break;
+      case "proposal":
+        setData(proposalPortfolio);
+        break;
+
+      default:
+        setData(webAppPortfolio);
+    }
+  }, [selected]);
+
+  console.log(data);
+
   return (
     <div className="portfolio" id="portfolio">
       <h1>Portfolio</h1>
@@ -39,14 +69,12 @@ export const Portfolio = () => {
         ))}
       </ul>
       <div className="container">
-        <div className="item">
-          <img src="" alt="" />
-          <h3>Sample App</h3>
-        </div>
-        <div className="item">
-          <img src="" alt="" />
-          <h3>Sample App</h3>
-        </div>
+        {data.map((d) => (
+          <div className="item" key={d.id}>
+            <img src={d.img} alt="" />
+            <h3>{d.title}</h3>
+          </div>
+        ))}
       </div>
     </div>
   );
