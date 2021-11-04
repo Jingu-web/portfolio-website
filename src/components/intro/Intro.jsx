af031a883961f5f81f1ad3e236e234bad1f2cf05
Jingu-web <multiNomaFeBe.rjrrr@gmail.com>
2021-11-04 18:19:21 +0900

    💄 Introページの作成

--- src/components/intro/Intro.jsx
+++ src/components/intro/Intro.jsx
@@ -1,5 +1,13 @@
 import "./intro.scss";
+import Mask from "../../assets/mask.jpg";
+import Video from "../../assets/video.mp4";
 
 export const Intro = () => {
-  return <div className="intro" id="intro"></div>;
+  return (
+    <div className="intro" id="intro">
+      <video src={Video} autoPlay loop muted type="mp4" />
+      <img src={Mask} className="mask" alt="" />
+      <h2>Hello.</h2>
+    </div>
+  );
 };
